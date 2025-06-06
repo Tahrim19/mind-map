adc4b4feacdf6b44b33547fe5b8f26d895a0eade
Tahrim19 <tarimbilal4@gmail.com>
2025-06-06 17:25:26 +0500

    cors error fixing

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://mind-map-ivory.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
