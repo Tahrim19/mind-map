4316fbbbd817919ae47ff4faaf2fd655aef9535f
Tahrim19 <tarimbilal4@gmail.com>
2025-06-06 16:35:53 +0500

    deployment changes

--- client/src/requests.ts
+++ client/src/requests.ts
@@ -1,10 +1,23 @@
+// export const requests = {
+//     createMap: " http://localhost:5000/api/mindmaps",
+//     deleteMap: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
+//     fetchAllMaps: "http://localhost:5000/api/mindmaps",
+//     fetchMap: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
+//     saveMap: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
+//     titleChange: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
+//     login: "http://localhost:5000/api/auth/",
+//     register: "http://localhost:5000/api/auth/register",
+// }
+
+
+const baseUrl = "https://mind-map-ivory.vercel.app";
 export const requests = {
-    createMap: " http://localhost:5000/api/mindmaps",
-    deleteMap: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
-    fetchAllMaps: "http://localhost:5000/api/mindmaps",
-    fetchMap: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
-    saveMap: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
-    titleChange: (mapId : String) => `http://localhost:5000/api/mindmaps/${mapId}`,
-    login: "http://localhost:5000/api/auth/",
-    register: "http://localhost:5000/api/auth/register",
+    createMap: `${baseUrl}/mindmaps`,
+    deleteMap: (mapId : String) => `${baseUrl}/mindmaps/${mapId}`,
+    fetchAllMaps: `${baseUrl}/mindmaps`,
+    fetchMap: (mapId : String) => `${baseUrl}/mindmaps/${mapId}`,
+    saveMap: (mapId : String) => `${baseUrl}/mindmaps/${mapId}`,
+    titleChange: (mapId : String) => `${baseUrl}/mindmaps/${mapId}`,
+    login: `${baseUrl}/auth/`,
+    register: `${baseUrl}/auth/register`,
 }
